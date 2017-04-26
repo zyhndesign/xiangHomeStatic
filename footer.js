78d286bd23d8046ccea57923dd7dd737e1832a4d
csboyty <csboyty@gmail.com>
2017-04-26 14:49:15 +0800

    update

--- footer.js
+++ footer.js
@@ -1,5 +1,5 @@
 document.write(['<footer class="zyFooter">',
-    'copyright&copyright;2015中意工业设计（湖南）有限责任公司<br>',
+    'copyright&copy;2015中意工业设计（湖南）有限责任公司<br>',
     '版权所有&nbsp;&nbsp;&nbsp;&nbsp;湘ICP<br><br>',
     '客服电话：0731-89082332&nbsp;&nbsp;&nbsp;&nbsp;联系我们：cidic@cidic.cn',
     '</footer>'].join(""));
